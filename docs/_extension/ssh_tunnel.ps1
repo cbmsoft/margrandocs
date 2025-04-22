f33362550a6bebaf06fd8fd9a15fb1e37dde798f
Cassio Menezes <cassio.menezes@outlook.com.br>
2025-04-22 19:18:03 -0300

    feat: ssh config

--- docs/_extension/ssh_tunnel.ps1
+++ docs/_extension/ssh_tunnel.ps1
@@ -1,9 +1,12 @@
 # Save this as "ssh_tunnel.ps1"
 $homeDirectory = $env:USERPROFILE
-$localPort = "1521"  # Replace with your desired local port
-$remoteHost = "localhost"  # Replace with the remote host
-$remotePort = "1521"  # Replace with the remote port
 $username = "tunneluser"  # Replace with your SSH username
 $sshServer = "demo.pwn.com.br"  # Replace with your SSH server 134.65.249.173
+$localPort1 = "1521"  # First local port
+$remoteHost1 = "localhost"  # Remote host for the first port
+$remotePort1 = "1521"  # Remote port for the first mapping
+$localPort2 = "10050"  # Second local port
+$remoteHost2 = "localhost"  # Remote host for the second port
+$remotePort2 = "10050"  # Remote port for the second mapping
 
-ssh -i ${homeDirectory}\.ssh\id_rsa -f -N -L ${localPort}:${remoteHost}:${remotePort} ${username}@${sshServer} > "C:\temp\ssh_log.txt" 2>&1
+ssh -i ${homeDirectory}\.ssh\id_rsa -f -N -L ${localPort1}:${remoteHost1}:${remotePort1} -L ${localPort2}:${remoteHost2}:${remotePort2} ${username}@${sshServer} > "C:\temp\ssh_log.txt" 2>&1
